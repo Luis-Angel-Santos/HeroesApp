fe6f79da09139f4a84cad53960549e1640f1056c
Luis Angel Santos <santosangel748@gmail.com>
2022-12-19 18:22:54 -0600

    Creando el menu de nuestra aplicacion

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
+  exports: [
+    MatIconModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatListModule
   ]
 })
 export class MaterialModule { }
